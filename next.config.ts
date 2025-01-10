dc58daa3a317cccb27870254a18405f5cff39eec
José Galdámez <josegaldamez1991@gmail.com>
2025-01-10 17:57:09 -0600

    mas cors

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,20 @@ const nextConfig: NextConfig = {
                 source: "/api/:path*",
                 headers: [
                     {
-                        key: "Access-Control-Allow-Credentials",
-                        value: "true",
-                    },
-                    {
                         key: "Access-Control-Allow-Origin",
                         value: "*",
                     },
                     {
+                        key: "Access-Control-Allow-Credentials",
+                        value: "true",
+                    },
+                    {
                         key: "Access-Control-Allow-Methods",
-                        value: "GET,DELETE,PATCH,POST,PUT",
+                        value: "GET,HEAD,OPTIONS,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
                     },
                 ],
             },
--- src/app/api/sendEmail/middleware.ts
+++ src/app/api/sendEmail/middleware.ts
@@ -5,6 +5,15 @@ export function middleware(req: NextRequest) {
     console.log(req.nextUrl);
     const res = NextResponse.next();
     res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT"
+    );
+    res.headers.append(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
     return res;
 }
 
